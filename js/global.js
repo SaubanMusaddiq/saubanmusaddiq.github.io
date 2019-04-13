57883e79e6d2659352fae2f8bad33dd74c2fda31
saubanmusaddiq <mohammedsauban.musaddiq@infibeam.net>
2019-04-13 17:40:03 +0530

    Box blur cursor

--- js/global.js
+++ js/global.js
@@ -96,5 +96,32 @@
 		createEventHandlers();
 	});
 
+	var $box = $('.box'),
+  	inter = 30,
+  	speed = 0;
+
+	function moveBox(e) {
+	  //TweenMax.killTweensOf();
+	  $box.each(function(index, val) {
+	   TweenLite.to($(this), 0.05, { css: { left: e.pageX, top: e.pageY},delay:0+(index/750)});
+	  });
+	}
+
+	$(window).on('mousemove', moveBox);
+
+	$box.each(function(index, val) {
+	    index = index + 1;
+	    TweenMax.set(
+	      $(this),{
+	        autoAlpha: 1 - (0.0333 * index),
+	        delay:0
+	      });
+	  });
+	  TweenMax.set(
+	    $('.text:nth-child(30)'), {
+	      autoAlpha: 1.5,
+	      delay: 0
+	    }
+	  );
 
 }());
