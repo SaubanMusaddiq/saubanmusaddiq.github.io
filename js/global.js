e21ed0f6688fb2323fa4cfffbbd6c1a94d48c0d6
sauban <musaddiq.sauban@gmail.com>
2020-07-27 08:21:32 -0700

    Analytics

--- js/global.js
+++ js/global.js
@@ -155,4 +155,31 @@
 				});
 		});
 
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-173640589-1', 'auto');
+		ga('send', 'pageview');
+
+		var tabmap = {
+			"1":"profile",
+			"2":"education",
+			"3":"experience",
+			"4":"projects",
+			"5":"skills",
+			"6":"resume"
+		}
+		$('.port-tab-menu a').on('click', function(event) {
+			  event.preventDefault();
+			  var $this = $(this),
+				data = $this.data('tab');
+				ga('send', {
+				  hitType: 'event',
+				  eventCategory: tabmap[data],
+				  eventAction: 'click',
+				  eventLabel: 'Tabs'
+				});
+		})
 }());
